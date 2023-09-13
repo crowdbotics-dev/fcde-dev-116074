ca4f705fd1afd74e1cc059dc8cd54eeffe9302da
Crowdbotics <engineering@crowdbotics.com>
2023-09-13 10:18:01 +0000

    Update store.

--- store/fcdedevAPI/api.js
+++ store/fcdedevAPI/api.js
@@ -1,120 +1,104 @@
-import axios from "axios";
+import axios from "axios"
 const fcdedevAPI = axios.create({
   baseURL: "https://fcde-dev-116074.botics.co",
-  headers: {
-    Accept: "application/json",
-    "Content-Type": "application/json"
-  }
-});
-
+  headers: { Accept: "application/json", "Content-Type": "application/json" }
+})
 function api_docs_schema_retrieve(payload) {
-  return fcdedevAPI.get(`/api-docs/schema/`, {
-    params: {
-      lang: payload.lang
-    }
-  });
+  return fcdedevAPI.get(`/api-docs/schema/`, { params: { lang: payload.lang } })
 }
-
 function api_v1_login_create(payload) {
-  return fcdedevAPI.post(`/api/v1/login/`, payload);
+  return fcdedevAPI.post(`/api/v1/login/`, payload)
 }
-
 function api_v1_rohit_list(payload) {
-  return fcdedevAPI.get(`/api/v1/rohit/`);
+  return fcdedevAPI.get(`/api/v1/rohit/`)
 }
-
 function api_v1_rohit_create(payload) {
-  return fcdedevAPI.post(`/api/v1/rohit/`, payload);
+  return fcdedevAPI.post(`/api/v1/rohit/`, payload)
 }
-
 function api_v1_rohit_retrieve(payload) {
-  return fcdedevAPI.get(`/api/v1/rohit/${payload.id}/`);
+  return fcdedevAPI.get(`/api/v1/rohit/${payload.id}/`)
 }
-
 function api_v1_rohit_update(payload) {
-  return fcdedevAPI.put(`/api/v1/rohit/${payload.id}/`, payload);
+  return fcdedevAPI.put(`/api/v1/rohit/${payload.id}/`, payload)
 }
-
 function api_v1_rohit_partial_update(payload) {
-  return fcdedevAPI.patch(`/api/v1/rohit/${payload.id}/`, payload);
+  return fcdedevAPI.patch(`/api/v1/rohit/${payload.id}/`, payload)
 }
-
 function api_v1_rohit_destroy(payload) {
-  return fcdedevAPI.delete(`/api/v1/rohit/${payload.id}/`);
+  return fcdedevAPI.delete(`/api/v1/rohit/${payload.id}/`)
 }
-
 function api_v1_signup_create(payload) {
-  return fcdedevAPI.post(`/api/v1/signup/`, payload);
+  return fcdedevAPI.post(`/api/v1/signup/`, payload)
 }
-
 function api_v1_vgdhf_list(payload) {
-  return fcdedevAPI.get(`/api/v1/vgdhf/`);
+  return fcdedevAPI.get(`/api/v1/vgdhf/`)
 }
-
 function api_v1_vgdhf_create(payload) {
-  return fcdedevAPI.post(`/api/v1/vgdhf/`, payload);
+  return fcdedevAPI.post(`/api/v1/vgdhf/`, payload)
 }
-
 function api_v1_vgdhf_retrieve(payload) {
-  return fcdedevAPI.get(`/api/v1/vgdhf/${payload.id}/`);
+  return fcdedevAPI.get(`/api/v1/vgdhf/${payload.id}/`)
 }
-
 function api_v1_vgdhf_update(payload) {
-  return fcdedevAPI.put(`/api/v1/vgdhf/${payload.id}/`, payload);
+  return fcdedevAPI.put(`/api/v1/vgdhf/${payload.id}/`, payload)
 }
-
 function api_v1_vgdhf_partial_update(payload) {
-  return fcdedevAPI.patch(`/api/v1/vgdhf/${payload.id}/`, payload);
+  return fcdedevAPI.patch(`/api/v1/vgdhf/${payload.id}/`, payload)
 }
-
 function api_v1_vgdhf_destroy(payload) {
-  return fcdedevAPI.delete(`/api/v1/vgdhf/${payload.id}/`);
+  return fcdedevAPI.delete(`/api/v1/vgdhf/${payload.id}/`)
 }
-
+function modules_articles_article_list(payload) {
+  return fcdedevAPI.get(`/modules/articles/article/`)
+}
+function modules_articles_article_create(payload) {
+  return fcdedevAPI.post(`/modules/articles/article/`, payload)
+}
+function modules_articles_article_retrieve(payload) {
+  return fcdedevAPI.get(`/modules/articles/article/${payload.id}/`)
+}
+function modules_articles_article_update(payload) {
+  return fcdedevAPI.put(`/modules/articles/article/${payload.id}/`, payload)
+}
+function modules_articles_article_partial_update(payload) {
+  return fcdedevAPI.patch(`/modules/articles/article/${payload.id}/`, payload)
+}
+function modules_articles_article_destroy(payload) {
+  return fcdedevAPI.delete(`/modules/articles/article/${payload.id}/`)
+}
 function rest_auth_login_create(payload) {
-  return fcdedevAPI.post(`/rest-auth/login/`, payload);
+  return fcdedevAPI.post(`/rest-auth/login/`, payload)
 }
-
 function rest_auth_logout_retrieve(payload) {
-  return fcdedevAPI.get(`/rest-auth/logout/`);
+  return fcdedevAPI.get(`/rest-auth/logout/`)
 }
-
 function rest_auth_logout_create(payload) {
-  return fcdedevAPI.post(`/rest-auth/logout/`);
+  return fcdedevAPI.post(`/rest-auth/logout/`)
 }
-
 function rest_auth_password_change_create(payload) {
-  return fcdedevAPI.post(`/rest-auth/password/change/`, payload);
+  return fcdedevAPI.post(`/rest-auth/password/change/`, payload)
 }
-
 function rest_auth_password_reset_create(payload) {
-  return fcdedevAPI.post(`/rest-auth/password/reset/`, payload);
+  return fcdedevAPI.post(`/rest-auth/password/reset/`, payload)
 }
-
 function rest_auth_password_reset_confirm_create(payload) {
-  return fcdedevAPI.post(`/rest-auth/password/reset/confirm/`, payload);
+  return fcdedevAPI.post(`/rest-auth/password/reset/confirm/`, payload)
 }
-
 function rest_auth_registration_create(payload) {
-  return fcdedevAPI.post(`/rest-auth/registration/`, payload);
+  return fcdedevAPI.post(`/rest-auth/registration/`, payload)
 }
-
 function rest_auth_registration_verify_email_create(payload) {
-  return fcdedevAPI.post(`/rest-auth/registration/verify-email/`, payload);
+  return fcdedevAPI.post(`/rest-auth/registration/verify-email/`, payload)
 }
-
 function rest_auth_user_retrieve(payload) {
-  return fcdedevAPI.get(`/rest-auth/user/`);
+  return fcdedevAPI.get(`/rest-auth/user/`)
 }
-
 function rest_auth_user_update(payload) {
-  return fcdedevAPI.put(`/rest-auth/user/`, payload);
+  return fcdedevAPI.put(`/rest-auth/user/`, payload)
 }
-
 function rest_auth_user_partial_update(payload) {
-  return fcdedevAPI.patch(`/rest-auth/user/`, payload);
+  return fcdedevAPI.patch(`/rest-auth/user/`, payload)
 }
-
 export const apiService = {
   api_docs_schema_retrieve,
   api_v1_login_create,
@@ -131,6 +115,12 @@ export const apiService = {
   api_v1_vgdhf_update,
   api_v1_vgdhf_partial_update,
   api_v1_vgdhf_destroy,
+  modules_articles_article_list,
+  modules_articles_article_create,
+  modules_articles_article_retrieve,
+  modules_articles_article_update,
+  modules_articles_article_partial_update,
+  modules_articles_article_destroy,
   rest_auth_login_create,
   rest_auth_logout_retrieve,
   rest_auth_logout_create,
@@ -142,4 +132,4 @@ export const apiService = {
   rest_auth_user_retrieve,
   rest_auth_user_update,
   rest_auth_user_partial_update
-};
+}
